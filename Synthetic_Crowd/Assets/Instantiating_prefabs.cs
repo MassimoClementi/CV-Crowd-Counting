e4fd9a12f326189f141421414973c913bb97cdd0
MassimoClementi <maxmax81811@gmail.com>
2019-10-25 09:39:48 +0200

    Changed some variables name + added more TODOs

--- Synthetic_Crowd/Assets/Instantiating_prefabs.cs
+++ Synthetic_Crowd/Assets/Instantiating_prefabs.cs
@@ -13,7 +13,7 @@ public class Instantiating_prefabs : MonoBehaviour{
     public GameObject myPrefab;
 
     // Define script variables accessible from Unity
-    public int x_max,y_max;
+    public int x_n,y_n;     //number of people in x and y directions
     public float x_spacing, y_spacing;
     public int pos_rand_100;  //contribute of the random position
     public int rot_base, rot_range;
@@ -27,34 +27,33 @@ public class Instantiating_prefabs : MonoBehaviour{
     // Start is called before the first frame update
     void Start()
     {
-        for (int x = 0; x < x_max; x++){
-             for (int y = 0; y < y_max; y++){
+        for (int x = 0; x < x_n; x++){
+             for (int y = 0; y < y_n; y++){
                 
                 //TODO: introduce other variable to make displacements of
                 //      whole rows(sure) and columns(maybe)
                 // CODE HERE
 
-                //TODO: holes may be present in the crowd -> no instantiation
-                // CODE HERE
-
                 //TODO: need to understand how to change animation (or animation start)
                 //      for each person to make them move asynchronously
+
+                //TODO: reintroduce shaders in Unity simulations
 
                 //Manage instantiation variables
                 // Placement
                     rand_temp = (float)(rnd.Next(0,pos_rand_100)-pos_rand_100/2)/100;   //cast needed
-                    x_instantiate = (x + rand_temp - x_max/2) * x_spacing;
+                    x_instantiate = (x + rand_temp - x_n/2) * x_spacing;
                     rand_temp = (float)(rnd.Next(0,pos_rand_100)-pos_rand_100/2)/100;
-                    y_instantiate = (y + rand_temp - y_max/2) * y_spacing;
+                    y_instantiate = (y + rand_temp - y_n/2) * y_spacing;
 
                 // Rotation
                     rand_temp = (float)(rnd.Next(0,2*rot_range)-rot_range);
                     rot_instantiate = Quaternion.AngleAxis(rot_base+rand_temp,Vector3.up);
 
                 //INSTANTIATE the person
-                GameObject person = Instantiate(myPrefab,
-                                                new Vector3(x_instantiate,0,y_instantiate),
-                                                rot_instantiate);
+                    GameObject person = Instantiate(myPrefab,
+                                                    new Vector3(x_instantiate,0,y_instantiate),
+                                                    rot_instantiate);
 
                 /*
                 //Get all the components of the GameObject
@@ -65,15 +64,17 @@ public class Instantiating_prefabs : MonoBehaviour{
                 */
 
                 //TODO: random gender
+                    //person.GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>().hide = true;
+                    //person.GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>().ChangeRace("HumanFemale");
+                    //UMA.CharacterSystem.DynamicCharacterAvatar dca = person.GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>();
+                    //dca.ChangeRace("SkyCar");
 
-                //person.GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>().hide = true;
+                //TODO: random clothes (according to the genders) -> i'd suggest arrays
+                // CODE HERE
 
-                //person.GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>().ChangeRace("HumanFemale");
-                //UMA.CharacterSystem.DynamicCharacterAvatar dca = person.GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>();
-                //dca.ChangeRace("SkyCar");
+                //TODO: change height and weight
+                // CODE HERE
 
-                //TODO: random clothes (according to the genders)
-                //      i'd suggest arrays
              }
         }
 
