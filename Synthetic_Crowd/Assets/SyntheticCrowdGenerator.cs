f1aa0c522fd8ae53fcac22bc8f17b9385f2d6fc3
MassimoClementi <maxmax81811@gmail.com>
2019-10-30 16:50:38 +0100

    Added full model color management, with palettes and tresholds

--- Synthetic_Crowd/Assets/SyntheticCrowdGenerator.cs
+++ Synthetic_Crowd/Assets/SyntheticCrowdGenerator.cs
@@ -31,10 +31,16 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
     System.Random rnd = new System.Random();
     float randTemp;
     float columnRandIndex = 0, rowRandCumulIndex = 0;
+    bool maleBool;
+    List<Color> palette_dark = new List <Color>(), palette_bright = new List <Color>(),
+                palette_hair_common = new List <Color>(), palette_hair_unusual = new List <Color>(),
+                palette_skin_color = new List <Color>();
 
 
     void Start(){
         for (int y = 0; y < yIstances; y++){
+
+            InitializeColorPalettes();
 
             // Random components evaluation
             columnRandIndex = (float)(rnd.Next(0,columnRand_100)-columnRand_100/2)/100;
@@ -58,7 +64,8 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
                     rotToInstantiate = Quaternion.AngleAxis(rotBase+randTemp,Vector3.up);
 
                 //  Gender
-                    if (rnd.Next(0,100)<femaleToMaleRatio_100){
+                    maleBool = rnd.Next(0,100)<femaleToMaleRatio_100;
+                    if (maleBool){
                         if(rnd.Next(0,100)<girlRatio_100) PrefabToUse = PrefabsGirl[Random.Range(0, PrefabsGirl.Length)];
                         else PrefabToUse = PrefabsFemale[Random.Range(0, PrefabsFemale.Length)];
                     }
@@ -70,17 +77,16 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
                                              new Vector3(xToInstantiate,0,yToInstantiate),
                                              rotToInstantiate) as GameObject);
 
-                // Clothes color
-                    var personDCA = crowd[personId].GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>();
-                    personDCA.characterColors.SetColor("Shirt",Color.black);
-
-                // Quick test code: it WORKS!
-                // crowd[person_id].GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>().hide = true;
-                // DEBUG_PrintAllComponents(crowd[person_id]);
-
-
-                // TODO: random clothes (according to the genders) -> i'd suggest arrays
-                // CODE HERE
+                // Change model colors
+                    var personColorList = crowd[personId].GetComponent<UMA.CharacterSystem.DynamicCharacterAvatar>()
+                                                    .characterColors;
+                    
+                    SetRandColor(personColorList,"Hair",palette_hair_common,palette_hair_unusual,75);   // Hair
+                    personColorList.SetColor("Skin",RandColorFromList(palette_skin_color));             // Skin color           
+                    SetRandColor(personColorList,"Shirt",palette_dark,palette_bright,90);               // Shirt
+                    SetRandColor(personColorList,"Shirt2",palette_dark,palette_bright,20);               // ...
+                    SetRandColor(personColorList,"Pants1",palette_dark,palette_bright,90);              // Pants
+                    SetRandColor(personColorList,"PantsAccent",palette_dark,palette_bright,30);         // ...
 
 
                 // TODO: change height and weight
@@ -99,6 +105,63 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
 
     }
 
+
+    bool ProbabEvaluation(int trsh){
+        // Compute the probab boolean value
+        return rnd.Next(0,100)<trsh;
+    }
+
+    void SetRandColor(UMA.CharacterSystem.DynamicCharacterAvatar.ColorValueList personColorList,
+                        string name,List<Color> palette_1,List<Color> palette_2,int trsh){
+        /*  Evaluate the given probability distribution and set the desired field with a random
+            color from the winning palette
+        */ 
+
+        if(ProbabEvaluation(trsh)){
+            personColorList.SetColor(name,RandColorFromList(palette_1));
+        } else {
+            personColorList.SetColor(name,RandColorFromList(palette_2));
+        }
+    }
+
+    Color RandColorFromList(List<Color> palette){
+        return palette[rnd.Next(0,palette.Count)];
+    }
+
+    void InitializeColorPalettes(){
+
+            palette_dark.Insert(0,Color.black);
+            palette_dark.Insert(1,Color.grey);
+            palette_dark.Insert(2,new Color(0.06f,0.15f,0.28f));  // dark blue
+            palette_dark.Insert(3,new Color(0.38f,0f,0.05f));     // dark red
+            palette_dark.Insert(4,new Color(0.02f,0.2f,0.1f));    // dark blue
+
+
+            //TODO: change these colors? Too bright?
+            palette_bright.Insert(0,Color.white);
+            palette_bright.Insert(1,Color.green);
+            palette_bright.Insert(2,Color.yellow);
+            palette_bright.Insert(3,Color.red);
+            palette_bright.Insert(4,Color.magenta);
+            palette_bright.Insert(5,Color.cyan);
+            palette_bright.Insert(6,Color.blue);
+
+            // Hair palettes
+            palette_hair_common.Insert(0,new Color(0.3f,0.16f,0.05f));  // brown
+            palette_hair_common.Insert(1,Color.black);
+            palette_hair_unusual.Insert(0,new Color(0.8f,0.5f,0.25f));   // blonde
+            palette_hair_unusual.Insert(1,new Color(0.5f,0.20f,0.15f));  // red
+            palette_hair_unusual.Insert(2,new Color(0.5f,0.5f,0.5f));    // white
+
+            //Skin palette (from white to dark)
+            palette_skin_color.Insert(0,new Color(1f,0.8f,0.7f));   //repeat white
+            palette_skin_color.Insert(0,new Color(1f,0.8f,0.7f));   //... here!
+            palette_skin_color.Insert(1,new Color(0.9f,0.7f,0.6f));     //REPEAT...
+            palette_skin_color.Insert(1,new Color(0.9f,0.7f,0.6f));     //... here!
+            palette_skin_color.Insert(2,new Color(0.7f,0.4f,0.3f));
+            palette_skin_color.Insert(3,new Color(0.5f,0.4f,0.2f));
+            palette_skin_color.Insert(4,new Color(0.4f,0.3f,0.1f));
+    }
 
     void DEBUG_PrintAllComponents(GameObject obj){
         // Custom function: get all the components of the GameObject and print them on Unity Logger
