8a59f61bcea3ff4241f10904c704afcfa5e90b98
MassimoClementi <maxmax81811@gmail.com>
2019-11-07 18:39:15 +0100

    Code clean-up

--- CODE/ComputeCrowdDensity.py
+++ CODE/ComputeCrowdDensity.py
@@ -14,7 +14,7 @@ import h5py
 n_neighbours = 8
 beta = 0.3  # density map generator parameter
 
-enable_plot_flag =  1   # set to 1 to enable matplotlib window
+enable_plot_flag =  1   # set to 1 to enable matplot window
 
 
 ''' FUNCTIONS '''
@@ -41,15 +41,15 @@ density_map = np.zeros((2*n_y,2*n_x)) # padded density map
 
 
 ''' MAIN PROGRAM'''
-print(">",len(head_positions),"reference head points found!\n")
+print(">",len(head_positions),"reference head points found!")
 
-plt.subplot(221)
+plt.subplot(211)
 for head in range(0,len(head_positions)):
 
     # Show reference head points
     plt.scatter(head_positions[head][0],head_positions[head][1])
 
-    #Computation of average neighbours distance
+    # Computation of average neighbours distance
     for i in range(0,len(head_positions)):
         distances[i] = np.linalg.norm(head_positions[head]-head_positions[i])
     distances = np.sort(distances)
@@ -67,15 +67,12 @@ for head in range(0,len(head_positions)):
     sh_x += pad_x - round(k_x/2)+1          # fixed
     sh_y = int(head_positions[head][1])
     sh_y += pad_y - round(k_y/2)+1
+
+    # Add kernel to padded density map
     density_map[sh_y:k_y+sh_y,sh_x:k_x+sh_x] += kernel
 
 # Cropping and flipping for final map
 final_density_map = density_map[pad_y:n_y+pad_y,pad_x:n_x+pad_x]
-
-# Masked Image for debugging
-masked = np.multiply(\
-    np.flip(plt.imread("./Sample_images/screen_854x429_2019-11-07_09-22-56.png")[:,:,0],axis=0),\
-    final_density_map)
 
 # Estimate the number of heads from density map
 print("> Total head points obtained from the density map: ",\
@@ -89,11 +86,7 @@ dataset = outh5.create_dataset('density',\
 # Plotting and visualization
 if enable_plot_flag==1:
     plt.xlim((0,n_x)); plt.ylim((0,n_y)); plt.title("Head Positions")
-    plt.subplot(222); plt.imshow(density_map)
-    plt.xlim((0,2*n_x)); plt.ylim((0,2*n_y)); plt.title("Padded Density Map")
-    plt.subplot(223); plt.imshow(final_density_map)
+    plt.subplot(212); plt.imshow(final_density_map)
     plt.xlim((0,n_x)); plt.ylim((0,n_y)); plt.title("Final Density Map")
-    plt.subplot(224); plt.imshow(masked)
-    plt.xlim((0,n_x)); plt.ylim((0,n_y)); plt.title("Masked Image")
 
     plt.show()
