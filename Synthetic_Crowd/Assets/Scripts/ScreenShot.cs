84ebc37723e7ba0a9a14e57c70da783eb58fc937
enp97 <56884415+enp97@users.noreply.github.com>
2019-11-08 14:28:51 +0100

    Adjust filename as "IMG_index"

--- Synthetic_Crowd/Assets/Scripts/ScreenShot.cs
+++ Synthetic_Crowd/Assets/Scripts/ScreenShot.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
  public class ScreenShot : MonoBehaviour {
     //public int resWidth = 2550; 
     //public int resHeight = 3300;
- 
-    public static string ScreenShotName(int width, int height) {
-        return string.Format("{0}/Savings/Screenshots/screen_{1}x{2}_{3}.png", 
-                            Application.dataPath, 
-                            width, height, 
-                            System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    
+    string IndexedFilename(string stub, string extension){
+        int index = 0;
+        string filename = null;
+        do{
+            index++;
+            filename = string.Format("{0}{1}.{2}", stub, index, extension);
+        } while(File.Exists(filename));
+        return filename;
+    }
+
+    void Save(string path, string content){
+        File.AppendAllText(path, content);
     }
   
     void LateUpdate() {
@@ -26,25 +35,29 @@ using UnityEngine;
             RenderTexture.active = rt;
             screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
             GetComponent<Camera>().targetTexture = null;
-            RenderTexture.active = null; // JC: added to avoid errors
+            RenderTexture.active = null;
             Destroy(rt);
 
+            // Get updated filename and save
             byte[] bytes = screenShot.EncodeToPNG();
-            string filename = ScreenShotName(resWidth, resHeight);
+            //byte[] bytes = screenShot.EncodeToJPG(); 
+            string stub = Application.dataPath+"/Savings/Screenshots/IMG_";
+            string filename = IndexedFilename(stub,"png"); //"png" or "jpg"
             System.IO.File.WriteAllBytes(filename, bytes);
 
             Debug.Log(string.Format("Took screenshot to: {0}", filename));
 
             // Save head positions
             int size = SyntheticCrowdGenerator.headPositions.Count;
-            string fn = "Savings/Positions/pos_"+System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")+".txt";
-            
+            string stb = Application.dataPath+"/Savings/Positions/IMG_";
+            string fn = IndexedFilename(stb,"txt");
+
             for(int i=0; i<size; i++){
                 Vector3 pos = GetComponent<Camera>().WorldToViewportPoint(SyntheticCrowdGenerator.headPositions[i]);
                 if( (pos.x >= 0 && pos.x <= 1) && (pos.y >= 0 && pos.y <= 1) && (pos.z > 0) ){
-                    SavePosition.Save(fn, 
-                                    ((int)(pos.x*resWidth)).ToString()+" "+
-                                    ((int)(pos.y*resHeight)).ToString()+"\n");
+                    Save(fn, 
+                        ((int)(pos.x*resWidth)).ToString()+" "+
+                        ((int)(pos.y*resHeight)).ToString()+"\n");
                 }
             }
             Debug.Log("Saved head positions");
