e8cb7b8596d1599cad9bfe68cd04df16981e17a4
enp97 <56884415+enp97@users.noreply.github.com>
2019-11-11 23:05:24 +0100

    Autonomous generation of crowd dataset + screenshot and save head position

--- Synthetic_Crowd/Assets/Scripts/ScreenShot.cs
+++ Synthetic_Crowd/Assets/Scripts/ScreenShot.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 using System.IO;
 using System.Text;
 
- public class ScreenShot : MonoBehaviour {
-    //public int resWidth = 2550; 
-    //public int resHeight = 3300;
-    
-    string IndexedFilename(string stub, string extension){
+ public class ScreenShot : MonoBehaviour {   
+    public static string IndexedFilename(string stub, string extension){
         int index = 0;
         string filename = null;
         do{
@@ -18,49 +15,49 @@ using System.Text;
         return filename;
     }
 
-    void Save(string path, string content){
+    public static void Save(string path, string content){
         File.AppendAllText(path, content);
+    }
+
+    public static void TakeScreenshot(Camera cam){
+        // Take screenshot
+        RenderTexture rt = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 24);
+        cam.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(cam.pixelWidth, cam.pixelHeight, TextureFormat.RGB24, false);
+        cam.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, cam.pixelWidth, cam.pixelHeight), 0, 0);
+        cam.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(rt);
+
+        // Get updated filename and save
+        byte[] bytes = screenShot.EncodeToJPG(); 
+        string stub = Application.dataPath+"/Savings/Screenshots/IMG_";
+        string filename = IndexedFilename(stub,"jpg");
+        System.IO.File.WriteAllBytes(filename, bytes);
+
+        Debug.Log(string.Format("Took screenshot to: {0}", filename));
+
+        // Save head positions
+        int size = SyntheticCrowdGenerator.headPositions.Count;
+        string stb = Application.dataPath+"/Savings/Positions/IMG_";
+        string fn = IndexedFilename(stb,"txt");
+
+        for(int i=0; i<size; i++){
+            Vector3 pos = cam.WorldToViewportPoint(SyntheticCrowdGenerator.headPositions[i]);
+            if( (pos.x >= 0 && pos.x <= 1) && (pos.y >= 0 && pos.y <= 1) && (pos.z > 0) ){
+                Save(fn, 
+                    ((int)(pos.x*cam.pixelWidth)).ToString()+" "+
+                    ((int)(pos.y*cam.pixelHeight)).ToString()+"\n");
+            }
+        }
+        Debug.Log("Saved head positions");
     }
   
     void LateUpdate() {
-        int resWidth = GetComponent<Camera>().pixelWidth;
-        int resHeight = GetComponent<Camera>().pixelHeight;
-
-        if (Input.GetKeyDown("k")) {
-            // Take screenshot
-            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
-            GetComponent<Camera>().targetTexture = rt;
-            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-            GetComponent<Camera>().Render();
-            RenderTexture.active = rt;
-            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
-            GetComponent<Camera>().targetTexture = null;
-            RenderTexture.active = null;
-            Destroy(rt);
-
-            // Get updated filename and save
-            byte[] bytes = screenShot.EncodeToPNG();
-            //byte[] bytes = screenShot.EncodeToJPG(); 
-            string stub = Application.dataPath+"/Savings/Screenshots/IMG_";
-            string filename = IndexedFilename(stub,"png"); //"png" or "jpg"
-            System.IO.File.WriteAllBytes(filename, bytes);
-
-            Debug.Log(string.Format("Took screenshot to: {0}", filename));
-
-            // Save head positions
-            int size = SyntheticCrowdGenerator.headPositions.Count;
-            string stb = Application.dataPath+"/Savings/Positions/IMG_";
-            string fn = IndexedFilename(stb,"txt");
-
-            for(int i=0; i<size; i++){
-                Vector3 pos = GetComponent<Camera>().WorldToViewportPoint(SyntheticCrowdGenerator.headPositions[i]);
-                if( (pos.x >= 0 && pos.x <= 1) && (pos.y >= 0 && pos.y <= 1) && (pos.z > 0) ){
-                    Save(fn, 
-                        ((int)(pos.x*resWidth)).ToString()+" "+
-                        ((int)(pos.y*resHeight)).ToString()+"\n");
-                }
-            }
-            Debug.Log("Saved head positions");
+        if (Input.GetKeyDown("s")) {
+            TakeScreenshot(GetComponent<Camera>());
         }
     }
 }
--- Synthetic_Crowd/Assets/Scripts/SyntheticCrowdGenerator.cs
+++ Synthetic_Crowd/Assets/Scripts/SyntheticCrowdGenerator.cs
@@ -8,6 +8,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+public struct CameraFeatures{
+    public Vector3 position;
+    public Quaternion rotation;
+    public CameraFeatures(float px, float py, float pz, float rx, float ry, float rz){
+        position.x = px;
+        position.y = py;
+        position.z = pz;
+        rotation = Quaternion.Euler(rx,ry,rz);
+    }
+}
 
 public class SyntheticCrowdGenerator : MonoBehaviour{
 
@@ -36,10 +48,13 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
     System.Random rnd = new System.Random();
     int personId = 0, nCrowd = 0, nCrowd_temp = 0;
     bool femaleBool,girlBool;
+    int[] crowdDensity = new int[] {50, 81, 100, 150, 200, 250}; //, 300, 400, 500, 800, 1000};
+    CameraFeatures[] cameras = new CameraFeatures[4];
+    int crowdDensityPos = 0, camerasPos = 0;
 
 
-    void Start(){
-
+    void Generate(){
+        personId = 0;
         // Local initialization
         float columnRandIndex = 0, rowRandCumulIndex = 0;
         InitializeColorPalettes();
@@ -51,10 +66,6 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
             rowRandCumulIndex += (float)(rnd.Next(0,rowRandCumul_100))/100;
 
              for (int x = 0; x < xIstances; x++){
-
-                //TODO: need to understand how to change animation (or animation start)
-                //      for each person to make them move asynchronously
-
 
                 // Manage instantiation variables
                 //  Placement
@@ -71,10 +82,10 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
                     femaleBool = rnd.Next(0,100)<femaleToMaleRatio_100;
                     if (femaleBool){
                         girlBool = rnd.Next(0,100)<girlRatio_100;
-                        if(girlBool) PrefabToUse = PrefabsGirl[Random.Range(0, PrefabsGirl.Length)];
-                        else PrefabToUse = PrefabsFemale[Random.Range(0, PrefabsFemale.Length)];
+                        if(girlBool) PrefabToUse = PrefabsGirl[UnityEngine.Random.Range(0, PrefabsGirl.Length)];
+                        else PrefabToUse = PrefabsFemale[UnityEngine.Random.Range(0, PrefabsFemale.Length)];
                     }
-                    else PrefabToUse = PrefabsMale[Random.Range(0, PrefabsMale.Length)];
+                    else PrefabToUse = PrefabsMale[UnityEngine.Random.Range(0, PrefabsMale.Length)];
 
                 // INSTANTIATE the person
                     crowd.Insert(personId,
@@ -119,7 +130,6 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
         // Show head positions
         RenderHeadPoints(); // to see head points go into play mode, pause it and inspect the GameObjects
     }
-
 
     bool ProbabEvaluation(int trsh){
         // Compute the probab boolean value
@@ -194,11 +204,56 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
         }
     }
 
+    void InitializeCameraFeature(){
+        cameras[0] = new CameraFeatures(0.0f,6.0f,4.0f,50.0f,180.0f,0.0f);
+        cameras[1] = new CameraFeatures(-6.0f,2.0f,0.0f,0.0f,90.0f,0.0f);
+        cameras[2] = new CameraFeatures(0.0f,3.0f,5.0f,20.0f,180.0f,0.0f);
+        cameras[3] = new CameraFeatures(-3.0f,2.5f,5.0f,20.0f,135.0f,0.0f);
+    }
 
+    IEnumerator ManagerGenerator(){
+        Camera cam = Camera.main;
+        InitializeCameraFeature();
+
+        while (crowdDensityPos < crowdDensity.Length){
+            // Destroy previous crowd (if existing)
+            if(crowd.Count != 0){
+                for(int k=0; k<crowd.Count; k++){
+                    Destroy(crowd[k]);
+                }
+            }
+            crowd.Clear();
+            headPositions.Clear();
+
+            // Compute number of instances per axis
+            if (crowdDensity[crowdDensityPos] <= 100){
+                xIstances = (int)(Math.Sqrt(crowdDensity[crowdDensityPos]));
+                zIstances = (int)(Math.Sqrt(crowdDensity[crowdDensityPos]));
+            }
+            else{
+                xIstances = 10;
+                zIstances = (int)(crowdDensity[crowdDensityPos]/10);
+            }
+            Generate();
+
+            // Set camera
+            cam.transform.position = cameras[camerasPos].position;
+            cam.transform.rotation = cameras[camerasPos].rotation;
+
+            yield return new WaitForSeconds(10.0f);
+            // Take screenshot
+            ScreenShot.TakeScreenshot(cam);
+
+            if (camerasPos == cameras.Length-1) crowdDensityPos++;
+            camerasPos = (camerasPos+1) % cameras.Length;
+        }
+    }
+    
     void Update(){
         Camera cam = Camera.main;
+
+        // Count number of people in camera FOV
         nCrowd = 0;
-        
         for (int i=0; i<personId; i++){
             Vector3 pos = cam.WorldToViewportPoint(headPositions[i]);
             if( (pos.x >= 0 && pos.x <= 1) && (pos.y >= 0 && pos.y <= 1) && (pos.z > 0) ){
@@ -207,6 +262,22 @@ public class SyntheticCrowdGenerator : MonoBehaviour{
         }
         if (nCrowd != nCrowd_temp) Debug.Log(nCrowd+" people are visible"); 
         nCrowd_temp = nCrowd;
+
+        // Destroy previous crowd (if existing) and create a new one
+        if (Input.GetKeyDown("n")) {
+            if(crowd.Count != 0){
+                for(int i=0; i<crowd.Count; i++){
+                    Destroy(crowd[i]);
+                }
+            }
+            crowd.Clear();            
+            Generate();
+        }
+
+        // Autonomously generate and save dataset 
+        if (Input.GetKeyDown("g")){
+            StartCoroutine(ManagerGenerator());
+        }
     }
 
 
