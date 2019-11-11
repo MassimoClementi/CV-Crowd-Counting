a69916b21a870cc0eb663b32942b17e5e9faeabf
MassimoClementi <maxmax81811@gmail.com>
2019-11-11 11:55:19 +0100

    Venv and comments in configure.sh

--- ResidualRegression-pytorch-master/configure.sh
+++ ResidualRegression-pytorch-master/configure.sh
@@ -3,7 +3,13 @@
 echo
 echo  "=>  CV-Crowd-Counting CONFIGURATION  <="
 
+# Double-check that the project venv is activated 
+source venv/bin/activate
+
+# Generate proper JSON of the synthetic dataset
 ./generateJSON.sh
+
+# Generate density maps and semantic maps
 python3 -W ignore ComputeCrowdDensity.py
 
 echo " [OK!] Ready to go"
