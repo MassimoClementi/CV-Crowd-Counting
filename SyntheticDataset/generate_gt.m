34fb2d5ac507ea4706a3fa092d11edd89fc63991
enp97 <56884415+enp97@users.noreply.github.com>
2019-11-24 13:42:17 +0100

    generate ground truth .mat + update .gitignore

---
+++ SyntheticDataset/generate_gt.m
@@ -0,0 +1,38 @@
+pathgt = 'ground_truth';
+gts = dir([pathgt filesep '*.txt']);
+ngts = length(gts);
+
+pathimg = 'images';
+images = dir([pathimg filesep '*.jpg']);
+nimages = length(images);
+
+for i = 1:ngts
+    fileID = fopen(strcat(pathgt, '/', gts(i).name),'r');
+    formatSpec = '%f %f';
+    sizeA = [2 Inf];
+    A = fscanf(fileID,formatSpec,sizeA);
+    A = A';
+    fclose(fileID);
+    
+    filename = strcat(pathimg,'/',images(i).name);
+    img = imread(filename);
+    [r, c, ch] = size(img);
+    
+    [value2, ~] = size(A);
+    
+    for row = 1:value2
+        A(row,2) = r - A(row,2);
+    end
+    
+    image_info = cell(1);
+    field1 = 'location';
+    value1 = A;
+    field2 = 'number';
+    image_info{1,1} = struct(field1, value1, field2, value2);
+    
+    name = gts(i).name;
+    name = strsplit(name,'.');
+    name = name{1,1};
+    filename = strcat(pathgt,'/GT_',name,'.mat');
+    save(filename,'image_info');
+end
